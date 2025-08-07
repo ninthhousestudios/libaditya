1f0f9e8cb8cf8963f66cc04da1f8c0830fee7e62
josh <j0sh4rp3@proton.me>
2025-08-07 16:38:19 -0400

    added LICENSE and copyright noticeds, so i think it should be good to go public. removed file pyphlocation.py would just had an old version of Location class in it

--- chtk2pyph.py
+++ chtk2pyph.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python
 
+#    This file is part of pyphemeris.
+#
+#    Copyright (c) 2025 Josh Harper <humanhaven@substack.com>
+#
+#    pyphemeris is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    pyphemeris is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with pyphemeris.  If not, see <https://www.gnu.org/licenses/>.
+
 import argparse
 import os
 import codecs
